0948da632cb166eeee9905e34efad127bcb9fadb
KaichenZhang <ac1102919258@gmail.com>
2016-11-17 22:25:17 -0200

    Add board place piece tests

--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -1,8 +1,8 @@
 package model;
 
+import com.google.common.base.Optional;
 import org.junit.Test;
 
-import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
 
 public class BoardTest {
@@ -29,8 +29,20 @@ public class BoardTest {
     Board board1 = board.placePiece(new Coordinate(3, 2), Piece.BLACK).get();
     Board board2 = board1.placePiece(new Coordinate(2, 2), Piece.WHITE).get();
     Board board3 = board2.placePiece(new Coordinate(2, 3), Piece.BLACK).get();
+    Board board4 = board3.placePiece(new Coordinate(4, 2), Piece.WHITE).get();
+    Board board5 = board4.placePiece(new Coordinate(5, 3), Piece.BLACK).get();
+    Board board6 = board5.placePiece(new Coordinate(2, 4), Piece.WHITE).get();
 
-    String[] actual = new String[] {board1.toString(), board2.toString(), board3.toString()};
+    Optional<Board> optionalBoard7 = board6.placePiece(new Coordinate(2, 4), Piece.BLACK);
+    Optional<Board> optionalBoard8 = board6.placePiece(new Coordinate(2, 4), Piece.WHITE);
+    Optional<Board> optionalBoard9 = board6.placePiece(new Coordinate(6, 7), Piece.BLACK);
+
+    String[] actual =
+        new String[] {board1.toString(), board2.toString(), board3.toString(), board4.toString(),
+            board5.toString(), board6.toString(),
+            optionalBoard7.isPresent() ? optionalBoard7.get().toString() : "",
+            optionalBoard8.isPresent() ? optionalBoard8.get().toString() : "",
+            optionalBoard9.isPresent() ? optionalBoard8.get().toString() : ""};
 
     String[] expected = new String[] {
         "00000000\n"
@@ -56,7 +68,34 @@ public class BoardTest {
             + "000BW000\n"
             + "00000000\n"
             + "00000000\n"
+            + "00000000\n",
+        "00000000\n"
             + "00000000\n"
+            + "00WB0000\n"
+            + "00WBB000\n"
+            + "00WWW000\n"
+            + "00000000\n"
+            + "00000000\n"
+            + "00000000\n",
+        "00000000\n"
+            + "00000000\n"
+            + "00WB0000\n"
+            + "00WBB000\n"
+            + "00WBW000\n"
+            + "000B0000\n"
+            + "00000000\n"
+            + "00000000\n",
+        "00000000\n"
+            + "00000000\n"
+            + "00WWW000\n"
+            + "00WWW000\n"
+            + "00WBW000\n"
+            + "000B0000\n"
+            + "00000000\n"
+            + "00000000\n",
+        "",
+        "",
+        ""
     };
 
     for (int index = 0; index < expected.length; index++) {
